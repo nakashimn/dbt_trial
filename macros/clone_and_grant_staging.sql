7c88a0fda217174c6241f5e262f8ddf7d1c8ace3
nakashimn <nakashima.naoto@gmail.com>
2024-08-05 09:28:18 -0700

    modify:temporally_clone

--- macros/clone_and_grant_staging.sql
+++ macros/clone_and_grant_staging.sql
@@ -1,66 +1,160 @@
 {% macro clone_and_grant_staging() %}
-// cloning
-;
-create or replace database staging clone production;
 
+////////////////////////////////////////////////////////////////////////////////
+// clone production to staging
+////////////////////////////////////////////////////////////////////////////////
+{{ print("[clone PRODUCTION to STAGING]") }}
+
+{% set clone_db %}
+create or replace database staging clone production
+{% endset %}
+
+{% do run_query(clone_db) %}
+
+-- ////////////////////////////////////////////////////////////////////////////////
 -- // grant privileges to DEVELOPER
--- grant USAGE on all schemas in database staging to role DEVELOPER;               -- 既存全Schemeの使用権限
--- grant USAGE on future schemas in database staging to role DEVELOPER;            -- 将来作られるSchemeの使用権限
--- grant USAGE on all stages in schema staging.raw to role DEVELOPER;              -- 既存Stageの私用権限
--- grant USAGE on future stages in schema staging.raw to role DEVELOPER;           -- 将来作られるStageの私用権限
--- grant SELECT on all tables  in schema staging.raw to role DEVELOPER;            -- 既存TableのSELECT権限
--- grant SELECT on future tables in schema staging.raw to role DEVELOPER;          -- 将来作られるTableのSELECT権限
--- grant SELECT on all views in schema staging.raw to role DEVELOPER;              -- 既存ViewのSELECT権限
--- grant SELECT on future views in schema staging.raw to role DEVELOPER;           -- 将来作られるViewのSELECT権限
--- grant SELECT on all tables  in schema staging.core to role DEVELOPER;           -- 既存TableのSELECT権限
--- grant SELECT on future tables in schema staging.core to role DEVELOPER;         -- 将来作られるTableのSELECT権限
--- grant SELECT on all views in schema staging.core to role DEVELOPER;             -- 既存ViewのSELECT権限
--- grant SELECT on future views in schema staging.core to role DEVELOPER;          -- 将来作られるViewのSELECT権限
--- grant SELECT on all tables  in schema staging.integration to role DEVELOPER;    -- 既存TableのSELECT権限
--- grant SELECT on future tables in schema staging.integration to role DEVELOPER;  -- 将来作られるTableのSELECT権限
--- grant SELECT on all views in schema staging.integration to role DEVELOPER;      -- 既存ViewのSELECT権限
--- grant SELECT on future views in schema staging.integration to role DEVELOPER;   -- 将来作られるViewのSELECT権限
+-- ////////////////////////////////////////////////////////////////////////////////
+-- {{ print("[grant privileges to DEVELOPER]") }}
+
+-- // 既存全Schemeの使用権限
+-- {% set grant_usage_on_all_schemas %}
+-- grant USAGE on all schemas in database staging to role DEVELOPER
+-- {% endset %}
+
+-- {% do run_query(grant_usage_on_all_schemas) %}
+
+-- -- // 将来作られるSchemeの使用権限
+-- -- {% set grant_usage_on_future_schemas %}
+-- -- grant USAGE on future schemas in database staging to role DEVELOPER
+-- -- {% endset %}
+
+-- -- {% do run_query(grant_usage_on_future_schemas) %}
 
 
--- // grant privileges to ADMINISTRATOR
--- grant ALL on database staging to role ADMINISTRATOR;
--- grant ALL on all schemas in database staging to role ADMINISTRATOR;             -- 既存全Schemeの使用権限
--- grant ALL on future schemas in database staging to role ADMINISTRATOR;          -- 将来作られるSchemeの使用権限
--- grant ALL on all stages in schema staging.raw to role ADMINISTRATOR;            -- 既存Stageの私用権限
--- grant ALL on future stages in schema staging.raw to role ADMINISTRATOR;         -- 将来作られるStageの私用権限
--- grant ALL on all tables in schema staging.raw to role ADMINISTRATOR;            -- 既存Tableの全権限
--- grant ALL on future tables in schema staging.raw to role ADMINISTRATOR;         -- 将来作られるTableの全権限
--- grant ALL on all views in schema staging.raw to role ADMINISTRATOR;             -- 既存Viewの全権限
--- grant ALL on future views in schema staging.raw to role ADMINISTRATOR;          -- 将来作られるViewの全権限
--- grant ALL on all tables in schema staging.core to role ADMINISTRATOR;           -- 既存Tableの全権限
--- grant ALL on future tables in schema staging.core to role ADMINISTRATOR;        -- 将来作られるTableの全権限
--- grant ALL on all tables in schema staging.core to role ADMINISTRATOR;           -- 既存Tableの全権限
--- grant ALL on future tables in schema staging.core to role ADMINISTRATOR;        -- 将来作られるTableの全権限
--- grant ALL on all views in schema staging.core to role ADMINISTRATOR;            -- 既存Viewの全権限
--- grant ALL on future views in schema staging.core to role ADMINISTRATOR;         -- 将来作られるViewの全権限
--- grant ALL on all tables in schema staging.integration to role ADMINISTRATOR;    -- 既存Tableの全権限
--- grant ALL on future tables in schema staging.integration to role ADMINISTRATOR; -- 将来作られるTableの全権限
--- grant ALL on all views in schema staging.integration to role ADMINISTRATOR;     -- 既存Viewの全権限
--- grant ALL on future views in schema staging.integration to role ADMINISTRATOR;  -- 将来作られるViewの全権限
+-- //  既存Stageの使用権限
+-- {% set grant_usage_on_all_stages %}
+-- grant USAGE on all stages in schema staging.raw to role DEVELOPER
+-- {% endset %}
 
--- // grant privileges to DBT_JOB
--- grant ALL on database staging to role DBT_JOB;
--- grant ALL on all schemas in database staging to role DBT_JOB;                   -- 既存全Schemeの使用権限
--- grant ALL on future schemas in database staging to role DBT_JOB;                -- 将来作られるSchemeの使用権限
--- grant ALL on all stages in schema staging.raw to role DBT_JOB;                  -- 既存Stageの私用権限
--- grant ALL on future stages in schema staging.raw to role DBT_JOB;               -- 将来作られるStageの私用権限
--- grant ALL on all tables in schema staging.raw to role DBT_JOB;                  -- 既存Tableの全権限
--- grant ALL on future tables in schema staging.raw to role DBT_JOB;               -- 将来作られるTableの全権限
--- grant ALL on all views in schema staging.raw to role DBT_JOB;                   -- 既存Viewの全権限
--- grant ALL on future views in schema staging.raw to role DBT_JOB;                -- 将来作られるViewの全権限
--- grant ALL on all tables in schema staging.core to role DBT_JOB;                 -- 既存Tableの全権限
--- grant ALL on future tables in schema staging.core to role DBT_JOB;              -- 将来作られるTableの全権限
--- grant ALL on all tables in schema staging.core to role DBT_JOB;                 -- 既存Tableの全権限
--- grant ALL on future tables in schema staging.core to role DBT_JOB;              -- 将来作られるTableの全権限
--- grant ALL on all views in schema staging.core to role DBT_JOB;                  -- 既存Viewの全権限
--- grant ALL on future views in schema staging.core to role DBT_JOB;               -- 将来作られるViewの全権限
--- grant ALL on all tables in schema staging.integration to role DBT_JOB;          -- 既存Tableの全権限
--- grant ALL on future tables in schema staging.integration to role DBT_JOB;       -- 将来作られるTableの全権限
--- grant ALL on all views in schema staging.integration to role DBT_JOB;           -- 既存Viewの全権限
--- grant ALL on future views in schema staging.integration to role DBT_JOB;        -- 将来作られるViewの全権限
+-- {% do run_query(grant_usage_on_all_stages) %}
+
+-- -- // 将来作られるStageの使用権限
+-- -- {% set grant_usage_on_future_stages %}
+-- -- grant USAGE on future stages in schema staging.raw to role DEVELOPER
+-- -- {% endset %}
+
+-- -- {% do run_query(grant_usage_on_future_stages) %}
+
+-- // 既存TableのSELECT権限
+-- {% set grant_select_on_all_tables_in_raw %}
+-- grant SELECT on all tables in schema staging.raw to role DEVELOPER
+-- {% endset %}
+
+-- {% do run_query(grant_select_on_all_tables_in_raw) %}
+
+-- -- // 将来作られるTableのSELECT権限
+-- -- {% set grant_select_on_future_tables_in_raw %}
+-- -- grant SELECT on future tables in schema staging.raw to role DEVELOPER
+-- -- {% endset %}
+
+-- -- {% do run_query(grant_select_on_future_tables_in_raw) %}
+
+-- // 既存ViewのSELECT権限
+-- {% set grant_select_on_all_views_in_raw %}
+-- grant SELECT on all views in schema staging.raw to role DEVELOPER
+-- {% endset %}
+
+-- {% do run_query(grant_select_on_all_views_in_raw) %}
+
+-- -- // 将来作られるViewのSELECT権限
+-- -- {% set grant_select_on_future_views_in_raw %}
+-- -- grant SELECT on future views in schema staging.raw to role DEVELOPER
+-- -- {% endset %}
+
+-- -- {% do run_query(grant_select_on_future_views_in_raw) %}
+
+-- // 既存TableのSELECT権限
+-- {% set grant_select_on_all_tables_in_core %}
+-- grant SELECT on all tables in schema staging.core to role DEVELOPER
+-- {% endset %}
+
+-- {% do run_query(grant_select_on_all_tables_in_core) %}
+
+-- -- // 将来作られるTableのSELECT権限
+-- -- {% set grant_select_on_future_tables_in_core %}
+-- -- grant SELECT on future tables in schema staging.core to role DEVELOPER
+-- -- {% endset %}
+
+-- -- {% do run_query(grant_select_on_future_tables_in_core) %}
+
+-- // 既存ViewのSELECT権限
+-- {% set grant_select_on_all_views_in_core %}
+-- grant SELECT on all views in schema staging.core to role DEVELOPER
+-- {% endset %}
+
+-- {% do run_query(grant_select_on_all_views_in_core) %}
+
+-- -- // 将来作られるViewのSELECT権限
+-- -- {% set grant_select_on_future_views_in_core %}
+-- -- grant SELECT on future views in schema staging.core to role DEVELOPER
+-- -- {% endset %}
+
+-- -- {% do run_query(grant_select_on_future_views_in_core) %}
+
+-- // 既存TableのSELECT権限
+-- {% set grant_select_on_all_tables_in_integration %}
+-- grant SELECT on all tables  in schema staging.integration to role DEVELOPER
+-- {% endset %}
+
+-- {% do run_query(grant_select_on_all_tables_in_integration) %}
+
+-- -- // 将来作られるTableのSELECT権限
+-- -- {% set grant_select_on_future_tables_in_integration %}
+-- -- grant SELECT on future tables in schema staging.integration to role DEVELOPER
+-- -- {% endset %}
+
+-- -- {% do run_query(grant_select_on_future_tables_in_integration) %}
+
+-- // 既存ViewのSELECT権限
+-- {% set grant_select_on_all_views_in_integration %}
+-- grant SELECT on all views in schema staging.integration to role DEVELOPER
+-- {% endset %}
+
+-- {% do run_query(grant_select_on_all_views_in_integration) %}
+
+-- -- // 将来作られるViewのSELECT権限
+-- -- {% set grant_select_on_future_views_in_integration %}
+-- -- grant SELECT on future views in schema staging.integration to role DEVELOPER
+-- -- {% endset %}
+
+-- -- {% do run_query(grant_select_on_future_views_in_integration) %}
+
+-- // 既存TableのSELECT権限
+-- {% set grant_select_on_all_tables_in_semantic %}
+-- grant SELECT on all tables  in schema staging.semantic to role DEVELOPER
+-- {% endset %}
+
+-- {% do run_query(grant_select_on_all_tables_in_semantic) %}
+
+-- -- // 将来作られるTableのSELECT権限
+-- -- {% set grant_select_on_future_tables_in_semantic %}
+-- -- grant SELECT on future tables in schema staging.semantic to role DEVELOPER
+-- -- {% endset %}
+
+-- -- {% do run_query(grant_select_on_future_tables_in_semantic) %}
+
+-- // 既存ViewのSELECT権限
+-- {% set grant_select_on_all_views_in_semantic %}
+-- grant SELECT on all views in schema staging.semantic to role DEVELOPER
+-- {% endset %}
+
+-- {% do run_query(grant_select_on_all_views_in_semantic) %}
+
+-- -- // 将来作られるViewのSELECT権限
+-- -- {% set grant_select_on_future_views_in_semantic %}
+-- -- grant SELECT on future views in schema staging.semantic to role DEVELOPER
+-- -- {% endset %}
+
+-- -- {% do run_query(grant_select_on_future_views_in_semantic) %}
+
 {% endmacro %}
