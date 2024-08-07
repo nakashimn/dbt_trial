e06eca0d4a079533ae82748c5a326b01b3cc2b18
nakashimn <nakashima.naoto@gmail.com>
2024-08-07 08:26:23 -0700

    modify:add_grant_method

--- macros/clone_and_grant_staging.sql
+++ macros/clone_and_grant_staging.sql
@@ -1,14 +1,29 @@
 {% macro clone_and_grant_staging() %}
 
-////////////////////////////////////////////////////////////////////////////////
-// clone production to staging
-////////////////////////////////////////////////////////////////////////////////
-{{ print("[clone PRODUCTION to STAGING]") }}
+    ////////////////////////////////////////////////////////////////////////////////
+    // clone production to staging
+    ////////////////////////////////////////////////////////////////////////////////
+    {{ print("[clone PRODUCTION to STAGING]") }}
 
-{% set clone_db %}
-create or replace database staging clone production
-{% endset %}
+    {% set clone_db %}
+    create or replace database staging clone production
+    {% endset %}
 
-{% do run_query(clone_db) %}
+    {% do run_query(clone_db) %}
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // grant USAGE on STAGING to ROLES
+    ////////////////////////////////////////////////////////////////////////////////
+    {% set target_roles = ["DEVELOPER"] %}
+
+    {% for target_role in target_roles %}
+        {{ print("[grant USAGE on STAGING to " ~ target_role ~ " ]") }}
+
+        {% set grant_usage_on_staging %}
+        grant USAGE on database STAGING to role {{ target_role }}
+        {% endset %}
+
+        {% do run_query(grant_usage_on_staging) %}
+    {% endfor %}
 
 {% endmacro %}
