316b8c766a846ec6e5a1ad033f6016eba7e1e6d3
nakashimn <nakashima.naoto@gmail.com>
2024-08-09 09:06:16 -0700

    modify:schema_name

--- macros/get_custom_schema.sql
+++ macros/get_custom_schema.sql
@@ -1,14 +1,15 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
-    {%- if (target.name == "prod") or (target.name == "stg") or (custom_schema_name is none) -%}
 
+    {%- if (custom_schema_name is none) -%}
         {{ default_schema }}
-
     {%- else -%}
-
-        {{ default_schema }}_{{ custom_schema_name | trim }}
-
+        {%- if (target.name == "prod") or (target.name == "stg") -%}
+            {{ custom_schema_name | trim }}
+        {%- else -%}
+            {{ default_schema }}_{{ custom_schema_name | trim }}
+        {%- endif -%}
     {%- endif -%}
 
 {%- endmacro %}
